57610546dcad6d1a5b8fe575db26ca752d0a2f36
Oleg <bezrukov-oa@nm.ru>
2021-07-16 15:11:32 +0300

    add footer and menu components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,33 +5,33 @@ import UserList from "./components/user";
 import axios from "axios";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      'users':[]
+    constructor(props) {
+        super(props);
+        this.state = {
+            'users': []
+        }
     }
-  }
 
-  componentDidMount() {
-    axios.get('http://127.0.0.1:8000/api/users')
-        .then(response => {
-            const users = response.data
-            this.setState(
-        {
-                'users':users
+    componentDidMount() {
+        axios.get('http://127.0.0.1:8000/api/users')
+            .then(response => {
+                const users = response.data
+                this.setState(
+                    {
+                        'users': users
+                    })
             })
-        })
-        .catch(error => console.log(error))
+            .catch(error => console.log(error))
 
-  }
+    }
 
-  render() {
-    return (
-      <div>
-          <UserList users={this.state.users} />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <UserList users={this.state.users}/>
+            </div>
+        )
+    }
 }
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,12 +5,37 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
+class Menu extends React.Component {
+    render() {
+        return (<h1>Меню</h1>)
+    }
+}
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Menu/>
+    </React.StrictMode>,
+    document.getElementById('menu')
+);
+
+class Footer extends React.Component {
+    render() {
+        return (<h2>Footer</h2>)
+    }
+}
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Footer/>
+    </React.StrictMode>,
+    document.getElementById('footer')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
