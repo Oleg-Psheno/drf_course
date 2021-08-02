a3c62f36e049fd714c3190ea76ca0c947857fd56
Oleg <bezrukov-oa@nm.ru>
2021-08-02 22:33:21 +0300

    create authentication form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,10 @@ import UserList from "./components/user";
 import ProjectList from "./components/projects";
 import ProjectTodoList from "./components/projectTodo";
 import TodoList from "./components/todo";
+import LoginForm from './components/LoginForm'
 import axios from "axios";
-import {HashRouter, Route, Switch, Redirect} from 'react-router-dom'
+import {HashRouter, Route, Switch, Redirect, Link} from 'react-router-dom'
+import Cookies from "universal-cookie";
 
 const Page404 = ({location}) => {
     return <div>Страница {location.pathname} не найдена</div>
@@ -17,12 +19,29 @@ class App extends React.Component {
         this.state = {
             'users': [],
             'projects': [],
-            'todos': []
+            'todos': [],
+            'token':''
         }
     }
 
-    componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/users')
+    get_token_from_storage(){
+        const cookie = new Cookies()
+        this.setState({'token':cookie.get('token')}, this.get_data)
+    }
+
+    get_headers(){
+        let headers = {
+            'Content-Type': 'application/json',
+        }
+        const cookie = new Cookies()
+
+        headers['Authorization'] = 'Token ' + cookie.get('token')
+        return headers
+    }
+
+    get_data(){
+        const headers = this.get_headers()
+        axios.get('http://127.0.0.1:8000/api/users', {headers})
             .then(response => {
                 const users = response.data
                 this.setState(
@@ -30,9 +49,12 @@ class App extends React.Component {
                         'users': users.results
                     })
             })
-            .catch(error => console.log(error))
+            .catch(error => {
+                this.setState({'users':[]})
+                console.log(error)
+            })
 
-        axios.get('http://127.0.0.1:8000/api/projects')
+        axios.get('http://127.0.0.1:8000/api/projects', {headers})
             .then(response => {
                 const projects = response.data
                 this.setState(
@@ -40,9 +62,12 @@ class App extends React.Component {
                         'projects': projects.results
                     })
             })
-            .catch(error => console.log(error))
+            .catch(error => {
+                this.setState({'projects':[]})
+                console.log(error)
+            })
 
-        axios.get('http://127.0.0.1:8000/api/todo')
+        axios.get('http://127.0.0.1:8000/api/todo', {headers})
             .then(response => {
                 const todos = response.data
                 this.setState(
@@ -50,17 +75,58 @@ class App extends React.Component {
                         'todos': todos.results
                     })
             })
+            .catch(error => {
+                this.setState({'todos':[]})
+                console.log(error)
+            })
+
+    }
+
+    componentDidMount() {
+        this.get_token_from_storage()
+    }
+
+    get_token(login, password) {
+        axios.post('http://127.0.0.1:8000/api-token-auth/', {'username':login,'password':password})
+            .then(response => {
+                localStorage.setItem('token', response.data.token)
+                const cookie = new Cookies()
+                cookie.set('token', response.data.token)
+                console.log(cookie.get('token'))
+                this.setState({'token': response.data.token}, this.get_data)
+            })
             .catch(error => console.log(error))
+    }
+
+    is_authenticated(){
+        return !!this.state.token
+    }
+
+    logout(){
+        const cookie = new Cookies()
+        cookie.set('token','')
+        this.setState({'token': ''}, this.get_data)
     }
 
     render() {
         return (
             <div>
                 <HashRouter>
+                    <nav>
+                        <ul>
+                            <li><Link to='/'>Users</Link></li>
+                            <li><Link to='/projects'>Projects</Link></li>
+                            <li><Link to='/todo'>Todo</Link></li>
+                            <li>
+                                {this.is_authenticated() ?  <button onClick={()=>this.logout()}>Logout</button> : <Link to='/login'>Login</Link>}
+                            </li>
+                        </ul>
+            </nav>
                     <Switch>
                         <Route exact path='/' component={() => <UserList users={this.state.users}/>}/>
                         <Route exact path='/projects' component={() => <ProjectList projects={this.state.projects}/>}/>
                         <Route exact path='/todo' component={() => <TodoList todos={this.state.todos} users={this.state.users}/>}/>
+                        <Route exact path='/login' component={() => <LoginForm get_token={(login,password)=>this.get_token(login,password)}/>}/>
                         <Route exact path='/project/:id'>
                            <ProjectTodoList todos={this.state.todos}/>
                         </Route>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,37 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Link,HashRouter} from 'react-router-dom'
+
 
 ReactDOM.render(
     <React.StrictMode>
         <App/>
     </React.StrictMode>,
     document.getElementById('root')
-);
-
-class Menu extends React.Component {
-    render() {
-        return (
-            <nav>
-                <ul>
-                    <HashRouter>
-                        <li><Link to='/'>Users</Link></li>
-                        <li><Link to='/projects'>Projects</Link></li>
-                        <li><Link to='/todo'>Todo</Link></li>
-                    </HashRouter>
-
-                </ul>
-            </nav>
-        )
-    }
-}
-
-ReactDOM.render(
-    <React.StrictMode>
-        <Menu/>
-    </React.StrictMode>,
-    document.getElementById('menu')
 );
 
 class Footer extends React.Component {
